de583546873571d2b0e1ef2f33c9bbfc03b73975
moirhira <ighermohamed@gmail.com>
2026-04-11 14:58:13 +0100

    setup the channel class stracture

---
+++ Channel.hpp
@@ -0,0 +1,41 @@
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+#include "Client.hpp"
+#include <string>
+#include <iostream>
+#include <vector>
+
+
+class Channel {
+    private:
+        std::string _name;
+        std::vector<Client*> _members;
+        std::vector<Client*> _operators;
+        std::string _pass;
+        std::string _topic;
+        int _usrLimit;
+        bool _inviteOnly;
+        bool _topicRestricted;
+        std::vector<Client*> _inviteLst;
+
+    public:
+        Channel(std::string& name,  Client* client);
+        ~Channel();
+
+        std::string getName() const;
+        std::string getPass() const;
+        std::string getTopic() const;
+        int getUserlimit() const;
+
+        void setPass(std::string pass);
+        void setTopic(std::string topic);
+        void setUsrlimit(int limit);
+
+        void addMember(Client* client);
+        bool isMember(Client* client);
+        bool isOpearator(Client* client);
+
+};
+
+
+#endif
