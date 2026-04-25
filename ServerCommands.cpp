0b32f3ed005537f41eb610eb62aef50be296f29c
moirhira <ighermohamed@gmail.com>
2026-04-25 16:54:21 +0100

    add Topic  in command folder

--- ServerCommands.cpp
+++ ServerCommands.cpp
@@ -276,108 +276,108 @@ bool isPreRegistrationCommand(const std::string &cmd)
 //     }
 // }
 
-void handlePrivmsg(Client *client, std::vector<std::string> params, std::vector<Client *> &clients, std::vector<Channel> &channels)
-{
-    if (params.size() == 0)
-    {
-        sendReply(client, 411, "No recipient given (PRIVMSG)", "PRIVMSG");
-        return;
-    }
-    if (params.size() == 1)
-    {
-        sendReply(client, 412, "No text to send", "PRIVMSG");
-        return;
-    }
-    std::string target = params[0];
-    std::string msg = params[1];
-    if (target[0] == '#')
-    {
-        for (size_t i = 0; i < channels.size(); i++)
-        {
-            if (channels[i].getName() == target)
-            {
-                if (!channels[i].isMember(client))
-                {
-                    sendReply(client, 442, "You are not on that channel", target);
-                    return;
-                }
+// void handlePrivmsg(Client *client, std::vector<std::string> params, std::vector<Client *> &clients, std::vector<Channel> &channels)
+// {
+//     if (params.size() == 0)
+//     {
+//         sendReply(client, 411, "No recipient given (PRIVMSG)", "PRIVMSG");
+//         return;
+//     }
+//     if (params.size() == 1)
+//     {
+//         sendReply(client, 412, "No text to send", "PRIVMSG");
+//         return;
+//     }
+//     std::string target = params[0];
+//     std::string msg = params[1];
+//     if (target[0] == '#')
+//     {
+//         for (size_t i = 0; i < channels.size(); i++)
+//         {
+//             if (channels[i].getName() == target)
+//             {
+//                 if (!channels[i].isMember(client))
+//                 {
+//                     sendReply(client, 442, "You are not on that channel", target);
+//                     return;
+//                 }
 
-                std::vector<Client *> members = channels[i].getMembers();
-                for (size_t j = 0; j < channels[i].getMembers().size(); j++)
-                {
-                    if (client->getFd() != members[j]->getFd())
-                    {
-                        std::string prvMsg = ":" + client->getNickname() + "!" + client->getUsername() + "@localhost PRIVMSG " + target + " :" + msg + "\r\n";
-                        members[j]->sendMessage(prvMsg);
-                    }
-                }
-                return;
-            }
-        }
-        sendReply(client, 403, "Channel doesn't exist", target);
-        return;
-    }
+//                 std::vector<Client *> members = channels[i].getMembers();
+//                 for (size_t j = 0; j < channels[i].getMembers().size(); j++)
+//                 {
+//                     if (client->getFd() != members[j]->getFd())
+//                     {
+//                         std::string prvMsg = ":" + client->getNickname() + "!" + client->getUsername() + "@localhost PRIVMSG " + target + " :" + msg + "\r\n";
+//                         members[j]->sendMessage(prvMsg);
+//                     }
+//                 }
+//                 return;
+//             }
+//         }
+//         sendReply(client, 403, "Channel doesn't exist", target);
+//         return;
+//     }
 
-    for (size_t i = 0; i < clients.size(); i++)
-    {
-        if (clients[i]->getNickname() == target)
-        {
-            std::string prvMsg = ":" + client->getNickname() + "!" +
-                                 client->getUsername() + "@localhost PRIVMSG " +
-                                 target + " :" + msg + "\r\n";
-            clients[i]->sendMessage(prvMsg);
-            return;
-        }
-    }
-    sendReply(client, 401, "No such nick/channel", target);
-}
+//     for (size_t i = 0; i < clients.size(); i++)
+//     {
+//         if (clients[i]->getNickname() == target)
+//         {
+//             std::string prvMsg = ":" + client->getNickname() + "!" +
+//                                  client->getUsername() + "@localhost PRIVMSG " +
+//                                  target + " :" + msg + "\r\n";
+//             clients[i]->sendMessage(prvMsg);
+//             return;
+//         }
+//     }
+//     sendReply(client, 401, "No such nick/channel", target);
+// }
 
-void handleTopic(Client *client, std::vector<std::string> params, std::vector<Channel> &channels)
-{
-    if (params.size() < 1)
-    {
-        sendReply(client, 461, "Not enough parameters", "TOPIC");
-        return;
-    }
-    if (params[0][0] != '#')
-    {
-        sendReply(client, 476, "Channel name should start with #", params[0]);
-        return;
-    }
-    for (size_t i = 0; i < channels.size(); i++)
-    {
-        if (channels[i].getName() == params[0])
-        {
-            if (!channels[i].isMember(client))
-            {
-                sendReply(client, 442, "You are not on that channel", params[0]);
-                return;
-            }
-            if (params.size() == 1)
-            {
-                if (!channels[i].getTopic().empty())
-                    sendReply(client, 332, channels[i].getTopic(), params[0]);
-                else
-                    sendReply(client, 331, "No topic is set", params[0]);
-                return;
-            }
-            if (channels[i].isTopicRestricted() && !channels[i].isOperator(client))
-            {
-                sendReply(client, 482, "You are not allowed to change the topic", params[0]);
-                return;
-            }
-            channels[i].setTopic(params[1]);
-            std::string topicMsg = ":" + client->getNickname() + "!" + client->getUsername() + "@localhost TOPIC " + params[0] + " :" + params[1] + "\r\n";
-            std::vector<Client *> members = channels[i].getMembers();
-            for (size_t j = 0; j < channels[i].getMembers().size(); j++)
-            {
-                members[j]->sendMessage(topicMsg);
-            }
-            return;
-        }
-    }
-    sendReply(client, 403, "Channel doesn't exist", params[0]);
-}
+// void handleTopic(Client *client, std::vector<std::string> params, std::vector<Channel> &channels)
+// {
+//     if (params.size() < 1)
+//     {
+//         sendReply(client, 461, "Not enough parameters", "TOPIC");
+//         return;
+//     }
+//     if (params[0][0] != '#')
+//     {
+//         sendReply(client, 476, "Channel name should start with #", params[0]);
+//         return;
+//     }
+//     for (size_t i = 0; i < channels.size(); i++)
+//     {
+//         if (channels[i].getName() == params[0])
+//         {
+//             if (!channels[i].isMember(client))
+//             {
+//                 sendReply(client, 442, "You are not on that channel", params[0]);
+//                 return;
+//             }
+//             if (params.size() == 1)
+//             {
+//                 if (!channels[i].getTopic().empty())
+//                     sendReply(client, 332, channels[i].getTopic(), params[0]);
+//                 else
+//                     sendReply(client, 331, "No topic is set", params[0]);
+//                 return;
+//             }
+//             if (channels[i].isTopicRestricted() && !channels[i].isOperator(client))
+//             {
+//                 sendReply(client, 482, "You are not allowed to change the topic", params[0]);
+//                 return;
+//             }
+//             channels[i].setTopic(params[1]);
+//             std::string topicMsg = ":" + client->getNickname() + "!" + client->getUsername() + "@localhost TOPIC " + params[0] + " :" + params[1] + "\r\n";
+//             std::vector<Client *> members = channels[i].getMembers();
+//             for (size_t j = 0; j < channels[i].getMembers().size(); j++)
+//             {
+//                 members[j]->sendMessage(topicMsg);
+//             }
+//             return;
+//         }
+//     }
+//     sendReply(client, 403, "Channel doesn't exist", params[0]);
+// }
 
 void handleMode(Client *client, std::vector<std::string> params, std::vector<Channel> &channels)
 {
--- commands/Topic.cpp
+++ commands/Topic.cpp
@@ -1,2 +1,49 @@
 #include "ServerCommands.hpp"
 #include "Server.hpp"
+
+void handleTopic(Client *client, std::vector<std::string> params, std::vector<Channel> &channels)
+{
+    if (params.size() < 1)
+    {
+        sendReply(client, 461, "Not enough parameters", "TOPIC");
+        return;
+    }
+    if (params[0][0] != '#')
+    {
+        sendReply(client, 476, "Channel name should start with #", params[0]);
+        return;
+    }
+    for (size_t i = 0; i < channels.size(); i++)
+    {
+        if (channels[i].getName() == params[0])
+        {
+            if (!channels[i].isMember(client))
+            {
+                sendReply(client, 442, "You are not on that channel", params[0]);
+                return;
+            }
+            if (params.size() == 1)
+            {
+                if (!channels[i].getTopic().empty())
+                    sendReply(client, 332, channels[i].getTopic(), params[0]);
+                else
+                    sendReply(client, 331, "No topic is set", params[0]);
+                return;
+            }
+            if (channels[i].isTopicRestricted() && !channels[i].isOperator(client))
+            {
+                sendReply(client, 482, "You are not allowed to change the topic", params[0]);
+                return;
+            }
+            channels[i].setTopic(params[1]);
+            std::string topicMsg = ":" + client->getNickname() + "!" + client->getUsername() + "@localhost TOPIC " + params[0] + " :" + params[1] + "\r\n";
+            std::vector<Client *> members = channels[i].getMembers();
+            for (size_t j = 0; j < channels[i].getMembers().size(); j++)
+            {
+                members[j]->sendMessage(topicMsg);
+            }
+            return;
+        }
+    }
+    sendReply(client, 403, "Channel doesn't exist", params[0]);
+}
