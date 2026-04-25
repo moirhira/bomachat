49c6303b021937f758a2e9a5a0e096216f702cf6
moirhira <ighermohamed@gmail.com>
2026-04-25 16:50:27 +0100

    add User in command folder

--- commands/User.cpp
+++ commands/User.cpp
@@ -1,2 +1,30 @@
 #include "ServerCommands.hpp"
 #include "Server.hpp"
+
+
+void handleUser(Client *client, const std::vector<std::string> params)
+{
+    if (!client->isAuth())
+    {
+        sendReply(client, 451, "You have not registered", "USER");
+        return;
+    }
+    if (client->isReg())
+    {
+        sendReply(client, 462, "You are already registred!", "USER");
+        return;
+    }
+    if (params.size() < 4)
+    {
+        sendReply(client, 461, "Not enough parameters", "USER");
+        return;
+    }
+    client->setUsername(params[0]);
+    client->setRealname(params[3]);
+    if (!client->getNickname().empty())
+    {
+        client->setRegistered(true);
+    }
+    if (client->isReg())
+        sendWelcome(client);
+}
