97d73df08b7d6af479fc5d7d9f7b807c98e8e972
moirhira <ighermohamed@gmail.com>
2026-05-06 15:33:07 +0100

    bonus/bot : finish

--- bot/main.cpp
+++ bot/main.cpp
@@ -9,16 +9,16 @@ int main(int ac, char **av)
         return 1;
     }
     std::string address = av[1];
-    int port = std::atoi(av[2]);
     std::string password = av[3];
+
+    int port = std::atoi(av[2]);
     if (port <= 0 || port > 65535)
     {
         std::cerr << "Invalid port!" << std::endl;
         return 1;
     }
-
     Bot bot(port, address);
-    
+
     std::string nick = "bot";
     std::string user = "botUser";
     std::string real = "boma";
